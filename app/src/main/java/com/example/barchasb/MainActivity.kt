5aebb24c18426af05231a05ea2dfb79b48ae28b2
saeed.samie <saeed.samie.74@gmail.com>
2024-11-08 21:06:54 +0330

    design: remove toolbar

--- app/src/main/java/com/example/barchasb/MainActivity.kt
+++ app/src/main/java/com/example/barchasb/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.barchasb
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.barchasb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +15,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//         تنظیم Toolbar به عنوان ActionBar
-        setSupportActionBar(binding.toolbar)
-
-        // تنظیم NavController
+        // تنظیم NavController بدون ActionBar
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        // تنظیم ActionBar با NavController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
