42cb5b17caf867066a12da770535677c5524f089
saeed.samie <saeed.samie.74@gmail.com>
2024-11-10 23:12:07 +0330

    design: night mode color bug fixed

--- app/src/main/java/com/example/barchasb/auth/SignUpFragment.kt
+++ app/src/main/java/com/example/barchasb/auth/SignUpFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -33,6 +34,11 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        val educationLevels = resources.getStringArray(R.array.education_options)
+        val educationAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, educationLevels)
+        educationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.educationSpinner.adapter = educationAdapter
         return binding.root
     }
 
